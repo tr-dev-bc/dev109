fbbcea9fe3c46394b4b26086c83571d284554573
tr-dev-bc <tristan.revells@bellevuecollege.edu>
2024-11-02 22:29:43 -0700

    add initial index and js file to artifact

---
+++ a3/rhombus.js
@@ -0,0 +1,55 @@
+// var rHeight =5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol ="*";
+
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    // Function 1 Defined Below
+    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    // Function 2 Defined Below
+    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+// Function 1
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = 0; i < pHeight; i++) {
+        rLine += "<p>";
+        // Create each line on the Rhombus
+        for (j = 0; j <= i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2) {
+                // even
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            } else {
+                // odd
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            }
+        }
+        rLine += "</p>";
+        // console.log(rLine);
+    }
+    document.getElementById("upRight").innerHTML = rLine;
+}
+
+// Function 2
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = pHeight; i > 0; i--) {
+        rLine += "<p>";
+        // Create each line on the Rhombus
+        for (j = 0; j < i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2) {
+                // even
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            } else {
+                // odd
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            }
+        }
+        rLine += "</p>";
+        // console.log(rLine);
+    }
+    document.getElementById("downRight").innerHTML = rLine;
+}
